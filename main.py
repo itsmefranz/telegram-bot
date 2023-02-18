058a57bec022cd15eeae8cdd8d206c84834b9c30
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 14:02:52 +0800

    imported more modules

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 bot = telegram.bot(token='TOKEN')
+updater = Updater(token='TOKEN', use_context=True) #Replace TOKEN with your token string
+dispatcher = updater.dispatcher
