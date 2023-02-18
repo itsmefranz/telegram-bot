2bb746781a3e965463183eca37b74568c9e157d8
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 18:55:26 +0800

    forgot a parenthesis for one function that became the reason for it to not work

--- main1.py
+++ main1.py
@@ -8,10 +8,10 @@ def start_command(update, context):
     update.message.reply_text('Type any random message here to get started!')
 
 def help_command(update, context):
-    update.message.reply_text('If you need help.. you can consult Google for it!')
+    update.message.reply_text('If you need help.. you can consult my buddy Google for it!')
 
 def handle_message(update, context):
-    text = str(update.message.text).lower
+    text = str(update.message.text).lower()
     response = R.sample_responses(text)
 
     update.message.reply_text(response)
@@ -24,7 +24,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("start", help_command))
+    dp.add_handler(CommandHandler("help", help_command))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
