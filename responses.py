517bf965a729aa23dc3679d43da8601c3bf451ce
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 15:21:40 +0800

    imported module and then defined user messages

---
+++ responses.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def sample_responses (input_text):
+    user_message = str(input_text).lower()
+
+    if user_message in ("hello", "hi", "sup"):
+        return "Hey! How is it going?"
+    
+    if user_message in ("who are you", "who are you?"):
+        return "I am Dani's bot!"
+    
+    if user_message in ("time", "time?"):
+        now=
