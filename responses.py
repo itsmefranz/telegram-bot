bb2408ab315e4914d759ea139883489c596353fc
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 18:54:09 +0800

    updated the list because it does not seem to work yet

--- responses.py
+++ responses.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def sample_responses (input_text):
+def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ("hello", "hi", "sup"):
@@ -15,4 +15,4 @@ def sample_responses (input_text):
 
         return str(date_time)
     
-    return "I can not understand you"
+    return "Sorry, i do not understand you"
