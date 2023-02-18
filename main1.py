68239d6e0a1709177902a5f12aab1817a8f9b8c1
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 15:44:12 +0800

    defined more functions regarding the main program

--- main1.py
+++ main1.py
@@ -4,3 +4,15 @@ import responses as R
 
 print("Bot started...")
 
+def start_command(update, context):
+    update.message.reply_text('Type your message here to get started!')
+
+def help_command(update, context):
+    update.message.reply_text('If you need help.. you can consult Google for it!')
+
+def handle_message(update, context):
+    text = str(update.message.text).lower
+    response = R.sample_responses(text)
+
+    update.message.reply_text(response)
+
