d03c366175c9beb892a5673a8e801a6287372b38
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 14:13:24 +0800

    created a hello function that sends message thru the bot

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 bot = telegram.bot(token='TOKEN')
-updater = Updater(token='TOKEN', use_context=True) #Replace TOKEN with your token string
+updater = Updater(token='TOKEN', use_context=True)
 dispatcher = updater.dispatcher
+
+def hello(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
+
+hello()
