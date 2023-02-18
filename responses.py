ce95a85cbecfe22a66b356bd1223f9481ac68841
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-02-18 19:34:50 +0800

    added a bit more of possible questions and responses for and by the bot

--- responses.py
+++ responses.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import requests
 
 def sample_responses(input_text):
     user_message = str(input_text).lower()
@@ -15,4 +16,18 @@ def sample_responses(input_text):
 
         return str(date_time)
     
-    return "Sorry, i do not understand you"
+    if user_message in ("what do you do", "what do you do?", "what are you", "what are you?"):
+        return "I am a telegram bot that can converse with users like you! :)"
+    
+    if user_message in ("can we play a game?", "do u know any games?", "play"):
+        print("Input six integers:")
+        nums = list(map(int, input().split()))
+        nums.sort()
+        nums.reverse()
+        print("After sorting the said ntegers:")
+        print(*nums)
+
+        return "There you go! Did you have fun?"
+    
+    return "Sorry, I do not understand you"
+
